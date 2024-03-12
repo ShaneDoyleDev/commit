ba97d30b25c88f2a3fed9ba99d2dfe47c91db368
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 13:40:46 +0000

    chore(commit/settings.py): set SECRET KEY to env variable

--- commit/settings.py
+++ commit/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1mb-(xoq1glm@^m3mj%$=l^5)pzh*v6uz)8=25n@!9on+%)*8c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
