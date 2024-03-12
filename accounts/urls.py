8463edd8138444382128cb47a8658a6c4fd8cf15
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 16:26:14 +0000

    feat(accounts/urls.py): add signup path

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from .views import SignUpView, CustomLoginView, CustomLogoutView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
