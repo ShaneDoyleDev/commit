d0d1aa9e52799df2defc4c30a17cd043ead65912
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 13:39:11 +0000

    feat(commit/settings.py): add import dependencies

--- commit/settings.py
+++ commit/settings.py
@@ -10,7 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
