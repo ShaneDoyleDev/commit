ee32bcd131cb7e96c91b743b0ff51dcc60713054
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 15:04:27 +0000

    chore(commit/settings.py): set allowed hosts

--- commit/settings.py
+++ commit/settings.py
@@ -34,8 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
