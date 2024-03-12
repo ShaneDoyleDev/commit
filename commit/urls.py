0bd30ec8509c0014e3043f142d1d09cb5485d805
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 15:28:06 +0000

    feat(commit/urls.py): add Summernote base url path

--- commit/urls.py
+++ commit/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
