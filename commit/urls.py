6091900fee318fca1f62a0a022b1ca9c6810140c
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 15:28:16 +0000

    feat(commit/urls.py): add blog base url path

--- commit/urls.py
+++ commit/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls')),
 ]
