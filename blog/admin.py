3f94bd714c1a983ec252eb3c60e5bf82341396fa
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 15:50:01 +0000

    feat(blog/admin.py): add PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment, Profile
+from .forms import ProfileForm
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Admin class for managing the Post model in the admin interface.
+    """
+
+    list_display = ['title', 'author', 'slug', 'category', 'created_on']
+    list_filter = ['created_on', 'category']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ['title']}
+    date_hierarchy = 'created_on'
+    ordering = ['created_on']
+    summernote_fields = ['content']
