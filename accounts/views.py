928ec53740a2baa5cc29e02c6a418a851dadd80f
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 16:27:18 +0000

    feat(accounts/views.py): import dependiencies

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# Third party imports
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+# Local application imports
+from .forms import CustomUserCreationForm
