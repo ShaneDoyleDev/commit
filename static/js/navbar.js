792ffc25c7e1e33e036f8c1ed7fc0ab3bd61702b
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 16:16:41 +0000

    feat(static/js/navbar.js): add event listener for navbar hamburger

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -5,3 +5,10 @@ const navbarDropdown = document.querySelector(".navbar-dropdown");
 const navbarHamburgerBtn = document.querySelector(".navbar-hamburger-btn");
 const navbarCloseBtn = document.querySelector(".navbar-close-btn");
 
+// ---------------------
+// Event Listeners
+// ---------------------
+navbarHamburgerBtn.addEventListener("click", function () {
+  navbarDropdown.classList.add("show-navbar-dropdown");
+});
+
