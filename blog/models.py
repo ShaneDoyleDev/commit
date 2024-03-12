d49546549aa6f1de498ee5db4923977f03bee145
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 15:34:40 +0000

    feat(blog/models.py): add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
+# Django imports
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.templatetags.static import static
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+# Third-party imports
+from cloudinary.models import CloudinaryField
+from cloudinary.uploader import destroy
+
+
+class Post(models.Model):
+    """Represents a blog post."""
+
+    CATEGORY_CHOICES = (
+        ('frontend', 'Front-End'),
+        ('backend', 'Back-End'),
+        ('design', 'UI/UX Design'),
+        ('devops', 'DevOps'),
+        ('perspectives', 'Thoughts & Perspectives'),
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    excerpt = models.TextField()
+    content = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    created_on = models.DateTimeField(auto_now_add=True)
+    post_image = CloudinaryField('image', folder='commit/post_images')
