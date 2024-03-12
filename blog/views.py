83becc879e6b44aa63fca06185d32b989067251f
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 15:55:04 +0000

    feat(blog/views.py): add homepage view function

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,15 @@ from django.views.generic.edit import UpdateView
 # Local application imports
 from .forms import PostForm, CommentForm, ProfileForm
 from .models import Post, Comment, Profile
+
+
+# ------- Site Views --------
+
+def homepage(request):
+    """
+    View function for the homepage of the site.
+    Returns the 4 most recent published posts.
+    """
+
+    recent_posts = Post.objects.order_by('-created_on')[:4]
+    return render(request, 'blog/homepage.html', {'recent_posts': recent_posts})
