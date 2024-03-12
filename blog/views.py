aa89bcfc387a9d1461096e36bd7d1eede0a33298
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 15:54:47 +0000

    feat(blog/views.py): add import dependencies

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# Django imports
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import UpdateView
 
-# Create your views here.
+# Local application imports
+from .forms import PostForm, CommentForm, ProfileForm
+from .models import Post, Comment, Profile
