0df323e93b72313626d228fd7b3c6303cf442e73
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 20:08:55 +0000

    feat(blog/static/js/category-menu.js): add DOM selectors

---
+++ blog/static/js/category-menu.js
@@ -0,0 +1,8 @@
+// ---------------------
+// DOM Selectors
+// ---------------------
+const categoryMenuItem = document.querySelectorAll(".category-menu-item");
+const categoryMenuSelector = document.querySelectorAll(
+  ".category-menu-selector"
+);
+const categoryMenuLine = document.querySelector(".category-menu-line");
