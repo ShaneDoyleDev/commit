95cb7be0f10c4a32cef858b520bb85901d57878a
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 16:15:51 +0000

    feat(static/js/navbar.js): add DOM selectors

---
+++ static/js/navbar.js
@@ -0,0 +1,7 @@
+// ---------------------
+// DOM Selectors
+// ---------------------
+const navbarDropdown = document.querySelector(".navbar-dropdown");
+const navbarHamburgerBtn = document.querySelector(".navbar-hamburger-btn");
+const navbarCloseBtn = document.querySelector(".navbar-close-btn");
+
