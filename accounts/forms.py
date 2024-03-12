d3d80b18348d72bb8847d45e0adfc741c3486dbf
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-12 16:30:18 +0000

    feat(accounts/forms.py): add CustomUserCreationForm class

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=255, required=False)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2',)
