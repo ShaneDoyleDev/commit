29e06fbac1c9ef1030357a681c2c73cdd4e57aea
Shane Doyle <shanedoyle1989@gmail.com>
2024-03-16 21:19:29 +0000

    refactor(blog/profile_urls.py): shorten profile update path

--- blog/profile_urls.py
+++ blog/profile_urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('edit/<int:pk>/', views.ProfileUpdateView.as_view(), name='update-profile'),
+    path('edit/<int:pk>/',
+         views.ProfileUpdateView.as_view(),
+         name='update-profile'
+         ),
     path('<int:pk>', views.ProfileDetailView.as_view(), name='profile'),
 ]
